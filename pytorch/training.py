Fix metrics computation in evaluate method

pytorch/training.py
@@ -24,11 +24,9 @@ def evaluate(model, data_loader, metrics: dict, weights: str = None, device: str
         metrics_dict = {}
         if any(predictions.sum(dim=1) != 1.0):
             predictions = softmax(predictions, dim=1)
-        # predictions = predictions.argmax(dim=1).to('cpu')
-        # labels = labels.argmax(dim=1).to('cpu')
         for name, metric_fct in metrics.items():
             if "matrix" not in name:
-                metrics_dict[name] = metric_fct(predictions.to("cpu"), labels.to("cpu")).item()
+                metrics_dict[name] = metric_fct(predictions.argmax(dim=1).to("cpu"), labels.argmax(dim=1).to("cpu")).item()
             else:
                 print("\nConfusion Matrix")
                 print(metric_fct(predictions.to("cpu"), labels.to("cpu")).numpy())