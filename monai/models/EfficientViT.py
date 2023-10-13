cf4c3115a23883a6df21a42a47d301e5884911fe
jesusalzate <alejoa0415@hotmail.com>
2023-10-13 13:12:25 +0200

    Adding comments to models

--- monai/models/EfficientViT.py
+++ monai/models/EfficientViT.py
@@ -5,29 +5,72 @@ from monai.networks.nets import ViT, EfficientNetBN
 
 class EfficientViT(nn.Module):
     """
-    Experimental architecture for image classification with an EfficientNet backbone for feature extraction
-    and a vanilla Vision Transformer for classification.
+    EfficientViT: An experimental architecture combining EfficientNet and Vision Transformer (ViT) 
+    for 3D image classification, designed for grayscale 3D volumes (e.g., structural brain MRIs).
 
-    Designed for grayscale 3D volumes (i.e. estructural brain MRI)
+    The model uses an EfficientNet backbone for feature extraction and a ViT for classification.
 
     Args:
-        model_name (str): model version (defaults to efficientnet-b0)
-        n_classes (int): number of output classes (defaults to 2)
-        pretrained_weights_path (str): path to EfficientNet pretrained weights (Optional, defaults to None)
+        model_name (str): Model version/name to use from the EfficientNet variants. Defaults to "efficientnet-b0".
+        n_classes (int): Number of output classes. Defaults to 2.
+        pretrained_weights_path (str): Path to the pretrained weights of EfficientNet (Optional). Defaults to None.
+
+    Forward Return:
+        x (Tensor): The classification logits.
+
+    Example:
+        model = EfficientViT(model_name='efficientnet-b0', n_classes=2, pretrained_weights_path=None)
     """
 
     def __init__(self, model_name: str = "efficientnet-b0", n_classes: int = 2, pretrained_weights_path: str = None):
+        """
+        Initialize the EfficientViT model with the given parameters.
+        """
         super(EfficientViT, self).__init__()
-        EfficientNet = EfficientNetBN(model_name=model_name, pretrained=False, progress=False, spatial_dims=3, in_channels=1, num_classes=n_classes)
+
+        # Instantiate the EfficientNet model
+        EfficientNet = EfficientNetBN(
+            model_name=model_name, 
+            pretrained=False, 
+            progress=False, 
+            spatial_dims=3, 
+            in_channels=1, 
+            num_classes=n_classes
+        )
+
+        # Load pretrained weights into EfficientNet if provided
         if pretrained_weights_path:
             EfficientNet.load_state_dict(torch.load(pretrained_weights_path))
+
+        # Use certain layers from EfficientNet for feature extraction
         layers = list(EfficientNet.children())[:3] + list(EfficientNet._blocks.children())[:2]
         self.features = nn.Sequential(*layers)
-        self.vit = ViT(in_channels=24, img_size=(23, 23, 23), patch_size=4, num_classes=n_classes, classification=True)
+
+        # Define the Vision Transformer model for classification
+        self.vit = ViT(
+            in_channels=24, 
+            img_size=(23, 23, 23), 
+            patch_size=4, 
+            num_classes=n_classes, 
+            classification=True
+        )
 
     def forward(self, x):
+        """
+        Forward pass through the model.
+
+        Args:
+            x (Tensor): Input image tensor of shape (batch_size, in_channels, depth, height, width).
+
+        Returns:
+            x (Tensor): Classification logits of shape (batch_size, n_classes).
+        """
+        # Extract features using EfficientNet
         x = self.features(x)
+
+        # Pass the features through the ViT model and retrieve the classification logits
         x = self.vit(x)[0]
+
         return x
 
 
--- monai/models/SwinUnetr_multitask.py
+++ monai/models/SwinUnetr_multitask.py
@@ -5,6 +5,27 @@ import torch.nn.functional as F
 
 
 class SwinUnetr_multitask(nn.Module):
+    """
+    SwinUnetr_multitask: A multi-task learning model using the SwinUnetr architecture, which is designed
+    to provide both classification and segmentation outputs given an input image.
+
+    Args:
+        n_classes (int): Number of output classes for the classification task. Defaults to 2.
+        img_size (tuple): Image size (Depth, Height, Width). Must be divisible by 32. Defaults to (96, 96, 96).
+        in_channels (int): Number of input image channels. Defaults to 1.
+        seg_channels (int): Number of output segmentation channels. Defaults to 14.
+        pretrained_weights (str): Path to the pretrained weights of the SwinUnetr model (Optional). Defaults to None.
+        feature_size (int): Feature size for the SwinUnetr model. Defaults to 48.
+        drop_rate (float): Dropout rate applied in SwinUnetr and the fully connected layers. Defaults to 0.0.
+
+    Forward Return:
+        class_out (Tensor): The classification logits.
+        logits_seg (Tensor): The segmentation map.
+
+    Example:
+        model = SwinUnetr_multitask(n_classes=3, img_size=(128, 128, 128), in_channels=1, pretrained_weights=None)
+    """
+    
     def __init__(
         self,
         n_classes: int = 2,
@@ -15,10 +36,14 @@ class SwinUnetr_multitask(nn.Module):
         feature_size: int = 48,
         drop_rate: float = 0.0,
     ):
+        """
+        Initialize the SwinUnetr_multitask model with the given parameters.
+        """
         super(SwinUnetr_multitask, self).__init__()
 
-        # Define Backbone (SwinUNETR)
         self.drop_rate = drop_rate
+
+        # Define the SwinUNETR model for feature extraction and segmentation.
         self.model = monai.networks.nets.SwinUNETR(
             img_size=img_size,
             in_channels=in_channels,
@@ -27,26 +52,40 @@ class SwinUnetr_multitask(nn.Module):
             use_v2=True,
             drop_rate=drop_rate,
         )
+
+        # Load pretrained weights if provided.
         if pretrained_weights:
             self.model.load_from(weights=torch.load(pretrained_weights))
 
+        # Extract the swinViT part of the SwinUNETR for feature extraction in classification.
         self.backbone = self.model.swinViT
 
+        # Define fully connected layers for classification.
         self.fc1 = nn.Linear(16 * feature_size, 8 * feature_size)
         self.fc2 = nn.Linear(8 * feature_size, 4 * feature_size)
         self.fc3 = nn.Linear(4 * feature_size, 2 * feature_size)
         self.fc4 = nn.Linear(2 * feature_size, feature_size)
-        # Define heads
 
+        # Define head for classification.
         self.out_class = nn.Linear(feature_size, n_classes)
 
     def forward(self, x_in):
+        """
+        Forward pass through the model.
+
+        Args:
+            x_in (Tensor): Input image tensor of shape (batch_size, in_channels, depth, height, width).
+
+        Returns:
+            class_out (Tensor): Classification logits of shape (batch_size, n_classes).
+            logits_seg (Tensor): Segmentation map.
+        """
+        # Extract features using the SwinViT backbone.
         hidden_states_out = self.backbone(x_in)
-        # print(x.shape)
+
         # Classification task
-        x1 = F.adaptive_avg_pool3d(hidden_states_out[4], (1, 1, 1)).view(
-            hidden_states_out[4].size(0), -1
-        )  # Global Average Pooling
+        # Perform Global Average Pooling and pass through the classification head.
+        x1 = F.adaptive_avg_pool3d(hidden_states_out[4], (1, 1, 1)).view(hidden_states_out[4].size(0), -1)
         x1 = F.dropout(F.relu(self.fc1(x1)), self.drop_rate)
         x1 = F.dropout(F.relu(self.fc2(x1)), self.drop_rate)
         x1 = F.dropout(F.relu(self.fc3(x1)), self.drop_rate)
@@ -54,6 +93,7 @@ class SwinUnetr_multitask(nn.Module):
         class_out = self.out_class(x1)
 
         # Segmentation task
+        # Pass through the SwinUNETR layers to produce the segmentation map.
         enc0 = self.model.encoder1(x_in)
         enc1 = self.model.encoder2(hidden_states_out[0])
         enc2 = self.model.encoder3(hidden_states_out[1])
