a7c2ecfcd859363696aea2ac3f247ce32f8abd46
jesusalzate <alejoa0415@hotmail.com>
2023-10-16 12:56:08 +0200

    New Parameter added

--- monai/models/EfficientViT.py
+++ monai/models/EfficientViT.py
@@ -5,7 +5,7 @@ from monai.networks.nets import ViT, EfficientNetBN
 
 class EfficientViT(nn.Module):
     """
-    EfficientViT: An experimental architecture combining EfficientNet and Vision Transformer (ViT) 
+    EfficientViT: An experimental architecture combining EfficientNet and Vision Transformer (ViT)
     for 3D image classification, designed for grayscale 3D volumes (e.g., structural brain MRIs).
 
     The model uses an EfficientNet backbone for feature extraction and a ViT for classification.
@@ -13,6 +13,8 @@ class EfficientViT(nn.Module):
     Args:
         model_name (str): Model version/name to use from the EfficientNet variants. Defaults to "efficientnet-b0".
         n_classes (int): Number of output classes. Defaults to 2.
+        in_channels_eff (int): Number of input channels for efficientnet. Defaults to 1.
+        in_channels_vit (int): Number of input channels for ViT. Depends on efficientnet final layer. Defaults to 24.
         pretrained_weights_path (str): Path to the pretrained weights of EfficientNet (Optional). Defaults to None.
 
     Forward Return:
@@ -22,7 +24,14 @@ class EfficientViT(nn.Module):
         model = EfficientViT(model_name='efficientnet-b0', n_classes=2, pretrained_weights_path=None)
     """
 
-    def __init__(self, model_name: str = "efficientnet-b0", n_classes: int = 2, pretrained_weights_path: str = None):
+    def __init__(
+        self,
+        model_name: str = "efficientnet-b0",
+        n_classes: int = 2,
+        in_channels_eff: int = 1,
+        in_channels_vit: int = 24,
+        pretrained_weights_path: str = None,
+    ):
         """
         Initialize the EfficientViT model with the given parameters.
         """
@@ -30,12 +39,12 @@ class EfficientViT(nn.Module):
 
         # Instantiate the EfficientNet model
         EfficientNet = EfficientNetBN(
-            model_name=model_name, 
-            pretrained=False, 
-            progress=False, 
-            spatial_dims=3, 
-            in_channels=1, 
-            num_classes=n_classes
+            model_name=model_name,
+            pretrained=True,
+            progress=False,
+            spatial_dims=3,
+            in_channels=in_channels_eff,
+            num_classes=n_classes,
         )
 
         # Load pretrained weights into EfficientNet if provided
@@ -43,16 +52,19 @@ class EfficientViT(nn.Module):
             EfficientNet.load_state_dict(torch.load(pretrained_weights_path))
 
         # Use certain layers from EfficientNet for feature extraction
-        layers = list(EfficientNet.children())[:3] + list(EfficientNet._blocks.children())[:2]
+        layers = (
+            list(EfficientNet.children())[:3]
+            + list(EfficientNet._blocks.children())[:2]
+        )
         self.features = nn.Sequential(*layers)
 
         # Define the Vision Transformer model for classification
         self.vit = ViT(
-            in_channels=24, 
-            img_size=(23, 23, 23), 
-            patch_size=4, 
-            num_classes=n_classes, 
-            classification=True
+            in_channels=in_channels_vit,
+            img_size=(32, 32, 8),
+            patch_size=4,
+            num_classes=n_classes,
+            classification=True,
         )
 
     def forward(self, x):
@@ -67,7 +79,7 @@ class EfficientViT(nn.Module):
         """
         # Extract features using EfficientNet
         x = self.features(x)
-
+        #print(x.shape)
         # Pass the features through the ViT model and retrieve the classification logits
         x = self.vit(x)[0]
 
@@ -75,9 +87,15 @@ class EfficientViT(nn.Module):
 
 
 def test():
-    model = EfficientViT(n_classes=3, pretrained_weights_path=None)
+    model = EfficientViT(
+        model_name="efficientnet-b7",
+        n_classes=3,
+        in_channels_eff=5,
+        in_channels_vit=48,
+        pretrained_weights_path=None,
+    )
     # print(model)
-    input = torch.randn(3, 1, 91, 91, 91)
+    input = torch.randn(3, 5, 128, 128, 32)
     out = model(input)
     print(out.shape)
     print(out)
