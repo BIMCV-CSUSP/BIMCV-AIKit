653a25d6bfee679a1d868b29d5914202d213759f
Alejandro Mora Rubio <morarubioa@gmail.com>
2023-10-25 11:05:12 +0200

    Update setup to find all packages

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 from bimcv_aikit import __version__
 
@@ -6,5 +6,5 @@ setup(
     name='bimcv_aikit',
     version=__version__,
     url='https://github.com/BIMCV-CSUSP/BIMCV-AIKit',
-    py_modules=['bimcv_aikit'],
+    py_modules=find_packages(),
 )
