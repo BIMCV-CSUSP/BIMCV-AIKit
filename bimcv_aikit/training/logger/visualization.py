420257bd36f495c910f5ef644f54bb5d587ddb97
Alejandro Mora Rubio <morarubioa@gmail.com>
2023-10-26 14:00:12 +0200

    Update writer to accept epoch from call

--- bimcv_aikit/training/logger/visualization.py
+++ bimcv_aikit/training/logger/visualization.py
@@ -59,10 +59,7 @@ class TensorboardWriter():
 
             def wrapper(tag, data, *args, **kwargs):
                 if add_data is not None:
-                    # add mode(train/valid) tag
-                    if name not in self.tag_mode_exceptions:
-                        tag = '{}/{}'.format(tag, self.mode)
-                    add_data(tag, data, self.step, *args, **kwargs)
+                    add_data(tag, data, *args, **kwargs)
             return wrapper
         else:
             # default action for returning methods defined in this class, set_step() for instance.
