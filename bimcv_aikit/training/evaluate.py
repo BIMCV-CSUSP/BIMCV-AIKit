485d1b5702e65473ed152b67ec7ba067893e2af6
Alejandro Mora Rubio <morarubioa@gmail.com>
2024-07-18 10:11:18 +0200

    Update evaluate script

--- bimcv_aikit/training/evaluate.py
+++ bimcv_aikit/training/evaluate.py
@@ -1,18 +1,16 @@
 import argparse
-import collections
 import importlib
 import json
+import types
 from functools import partial
 
 import numpy as np
 import torch
+from prettytable import PrettyTable
 
 from ..metrics.BaseMetric import BaseMetric
-from ..metrics.segmentation.metrics_segmentation import (
-    metrics_segmentation_constructor_monai,
-)
 from . import trainer as module_trainer
-from .parse_config import ConfigParser
+from .parse_config import ConfigParser, CustomArgs
 from .utils import prepare_device
 
 # fix random seeds for reproducibility
@@ -37,7 +35,6 @@ def main():
     )
 
     # custom cli options to modify configuration from default values given in json file.
-    CustomArgs = collections.namedtuple("CustomArgs", "flags type target")
     options = [
         CustomArgs(["--lr", "--learning_rate"], type=float, target="optimizer;args;lr"),
         CustomArgs(
@@ -61,16 +58,16 @@ def main():
 
     # prepare for (multi-device) GPU training
     device, device_ids = prepare_device(config["n_gpu"])
-    model = model.to(device)
+    model = model.to(device)  # type: ignore
     if len(device_ids) > 1:
         model = torch.nn.DataParallel(model, device_ids=device_ids)
 
     # get function handles of loss and metrics
-    if data_loader.class_weights is None:
+    if data_loader.class_weights is None:  # type: ignore
         criterion = config.init_obj("loss")
     else:
         criterion = config.init_obj(
-            "loss", **{"weight": torch.tensor(data_loader.class_weights).to(device)}
+            "loss", **{"weight": torch.tensor(data_loader.class_weights).to(device)}  # type: ignore
         )
 
     # build optimizer, learning rate scheduler. delete every lines containing lr_scheduler for disabling scheduler
@@ -82,21 +79,20 @@ def main():
         else None
     )
 
-    train_loader = data_loader(config["data_loader"]["partitions"]["train"])
-    valid_loader = data_loader(config["data_loader"]["partitions"]["val"])
+    train_loader = data_loader(config["data_loader"]["partitions"]["train"])  # type: ignore
+    valid_loader = data_loader(config["data_loader"]["partitions"]["val"])  # type: ignore
 
     metrics = {}
     for name, met in config["metrics"].items():
-        metric = partial(
-            getattr(importlib.import_module(met["module"]), met["type"]), **met["args"]
-        )
-
-        if "monai" in met["module"]:
-            metrics[name] = metrics_segmentation_constructor_monai(
-                original_metric=metric
+        element = getattr(importlib.import_module(met["module"]), met["type"])
+        if isinstance(element, types.FunctionType):
+            metric = partial(
+                getattr(importlib.import_module(met["module"]), met["type"]),
+                **met["args"],
             )
         else:
-            metrics[name] = BaseMetric(metric)
+            metric = element(**met["args"])
+        metrics[name] = BaseMetric(metric)
 
     Trainer = getattr(module_trainer, config["trainer"]["type"])
     trainer = Trainer(
@@ -128,7 +124,7 @@ def main():
 
     del train_loader, valid_loader
 
-    test_loader = data_loader(config["data_loader"]["partitions"]["test"])
+    test_loader = data_loader(config["data_loader"]["partitions"]["test"])  # type: ignore
     if test_loader:
         test_predictions, test_results = trainer.evaluate(
             test_loader, load_best_weights=False
@@ -138,6 +134,22 @@ def main():
 
     with open(f"{config.log_dir}/results.json", "w") as json_file:
         json.dump(results, json_file, ensure_ascii=False, indent=4)
+
+    table = PrettyTable()
+    table.title = "Final Performance Metrics"
+    metrics = list(results["Train Metrics"].keys())
+    table.add_column("Metrics", metrics)
+    train_values = [f"{value:.4f}" for _, value in results["Train Metrics"].items()]
+    table.add_column("Train", train_values)
+    if "Validation Metrics" in results:
+        validation_values = [
+            f"{value:.4f}" for _, value in results["Validation Metrics"].items()
+        ]
+        table.add_column("Validation", validation_values)
+    if "Test Metrics" in results:
+        test_values = [f"{value:.4f}" for _, value in results["Test Metrics"].items()]
+        table.add_column("Test", test_values)
+    logger.info(table)
 
 
 if __name__ == "__main__":
